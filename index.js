92364d6dc79d4be9d6f4fd4c4b0072a254ee65a2
breannams <breannamarie_s@hotmail.com>
2021-07-08 22:29:43 -0400

    commit tree 10be559465d3a2059303e5f7e9a9969b401aefab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+function mapToNegativize(sourceArray){
+   return  sourceArray.map(arr => arr * -1)
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(arr => arr)
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(arr => arr * 2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(arr => arr ** 2)
+}
+
+
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reduce = function(accumulator, element){return accumulator + element }
+    return sourceArray.reduce(reduce, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+    const reduce = function(accumulator, element){
+        if(!!accumulator === true && !!element === true){
+        return true;
+    }
+        else{
+            return false;
+        }
+        }
+        return sourceArray.reduce(reduce,true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reduce = function(accumulator, element){
+        if (accumulator === true){
+            return true;
+        }
+        else {
+            return !!element
+        }
+    }
+    return sourceArray.reduce(reduce, false)
+}
